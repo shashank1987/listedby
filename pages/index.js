bad8ce0b812bc6fcc21c655608dafb88a00fba5c
shash87 <shashikant.naik87@gmail.com>
2020-03-05 19:27:58 +0530

    Under Construction Page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 export default function Index() {
     return (
       <div>
-        <h1>Website Under Construction</h1>
+        <h1>Listedby</h1>
+        <h2>Website Under Construction</h2>
+        <style jsx>{`
+		  h1 {
+            font-size: 3rem;
+            text-align:center;
+          },
+          h2 {
+            text-align:center;
+          }
+		`}</style>
       </div>
     );
   }
