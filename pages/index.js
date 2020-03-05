ff4e2d954e4aa7176f0c244cbf2e264e25ff1314
shash87 <shashikant.naik87@gmail.com>
2020-03-05 19:40:22 +0530

    Under Construction Page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import Head from 'next/head'
+
 export default function Index() {
     return (
       <div>
+        <Head>
+        <title>Listedby UK</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" ></link>
+
+        </Head>
         <h1>Listedby</h1>
         <h2>Website Under Construction</h2>
         <style jsx>{`
